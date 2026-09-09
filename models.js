e7d8645ff6b7163a26b7f85e32c34631116c2264
WebAIPocket <ffox211310@gmail.com>
2026-09-09 10:00:55 +0900

    Create models.js

---
+++ models.js
@@ -0,0 +1,8 @@
+export const MODELS = {
+  "Q0.5B": "Qwen2.5-0.5B-Instruct-q4f16_1-MLC",
+  "Q1.5B": "Qwen2.5-1.5B-Instruct-q4f16_1-MLC",
+  "Q3B": "Qwen2.5-3B-Instruct-q4f16_1-MLC",
+  "Q7B": "Qwen2.5-7B-Instruct-q4f16_1-MLC",
+  "G2B-jpn": "gemma-2-2b-jpn-it-q4f16_1-MLC",
+  "G2B-jpnHv": "gemma-2-2b-jpn-it-q4f32_1-MLC",
+};

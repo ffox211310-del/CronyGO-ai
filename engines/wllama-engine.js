82c19386968ffd1b49a4a1d72319b8324d57f84a
WebAIPocket <ffox211310@gmail.com>
2026-09-23 13:24:48 +0900

    Update wllama-engine.js

--- engines/wllama-engine.js
+++ engines/wllama-engine.js
@@ -1,3 +1,7 @@
+import { MODELS } from "../models.js";
+
+import { Wllama } from "https://cdn.jsdelivr.net/npm/@wllama/wllama@3.5.1/esm/index.js";
+
 export class WllamaEngine {
   id = "wllama";
   supportsHotStop = true;

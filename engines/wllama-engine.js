27a444ddb393364a438aaa45754c60fde6cb6895
WebAIPocket <ffox211310@gmail.com>
2026-09-23 13:11:52 +0900

    Add supportsHotStop property to WllamaEngine

--- engines/wllama-engine.js
+++ engines/wllama-engine.js
@@ -1,5 +1,6 @@
 export class WllamaEngine {
   id = "wllama";
+  supportsHotStop = true;
   wllama = null;
   WASM_URL = "https://cdn.jsdelivr.net/npm/@wllama/wllama@3.5.1/esm/wasm/wllama.wasm";
   _abortFn = null;

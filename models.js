89aa52755a3a9113c09575819bbd0d65aaf400d6
WebAIPocket <ffox211310@gmail.com>
2026-09-23 17:58:46 +0900

    Update models.js

--- models.js
+++ models.js
@@ -3,6 +3,7 @@ export const MODELS = {
   "Q1.5B": "Qwen2.5-1.5B-Instruct-q4f16_1-MLC",
   "Q3B": "Qwen2.5-3B-Instruct-q4f16_1-MLC",
   "Q7B": "Qwen2.5-7B-Instruct-q4f16_1-MLC",
+  "Sm135B": "SmolLM2-135M-Instruct-q0f16-MLC",
   "G2B-jpn": "gemma-2-2b-jpn-it-q4f16_1-MLC",
   "G2B-jpnHv": "gemma-2-2b-jpn-it-q4f32_1-MLC",
   "Qwen-0.5B": "https://huggingface.co/Qwen/Qwen2.5-0.5B-Instruct-GGUF/resolve/main/qwen2.5-0.5b-instruct-q4_k_m.gguf",
